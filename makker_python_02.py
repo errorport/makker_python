bd560b22b37431f8e5eb9d6cd71ef447693363cb
Gergely Bencsik <becsegergely@gmail.com>
2018-11-18 19:30:49 +0100

    Harmadik fixed.

--- makker_python_02.py
+++ makker_python_02.py
@@ -2,19 +2,30 @@ import matplotlib.pyplot as plt
 import matplotlib.image as image
 import numpy as np
 
-scaling = 4
+scaling = 8
+_cmap="cool"
+pics = 10
 
-matrix = np.zeros([34,34])
+matrix = np.zeros([62,62])
 
 print(matrix)
 print(matrix.shape)
 
-for i in range(32):
+for i in range(pics//2):
 	for y in range(matrix.shape[0]):
 		for x in range(matrix.shape[1]):
-			matrix[y,x] = ((y+x) % i) * (x+y)
+			matrix[y,x] = ((y+x) % (i+2)) * (x+y)
 	matrix_to_export=np.kron(matrix, np.ones((scaling, scaling)))
-	image.imsave(fname="makker-sakk_"+str(i)+".png", arr=matrix_to_export, cmap="Greys_r")
+	imnum = '{:03d}'.format(i)
+	image.imsave(fname="makker-sakk_"+imnum+".png", arr=matrix_to_export, cmap=_cmap)
+
+for i in range(pics//2):
+	for y in range(matrix.shape[0]):
+		for x in range(matrix.shape[1]):
+			matrix[y,x] = ((y+x) % ((pics//2)-i+2)) * (x+y)
+	matrix_to_export=np.kron(matrix, np.ones((scaling, scaling)))
+	imnum = '{:03d}'.format(i+pics//2-1)
+	image.imsave(fname="makker-sakk_"+imnum+".png", arr=matrix_to_export, cmap=_cmap)
 
 print(matrix)
 plt.imshow(matrix, interpolation="none", cmap="Greys_r")
