7b047b1d5aa0e4e35320e82bd4bb6591a9939e10
Gergely Bencsik <becsegergely@gmail.com>
2018-12-02 14:50:54 +0100

    5th class

---
+++ makker_python_04.py
@@ -0,0 +1,53 @@
+from textblob import TextBlob
+
+text1 = "It's a good thing. I like it."
+text2 = "It's a bad thing. I don't like it."
+
+text_888 = '''
+Mielőtt elkezdeném én is az egyszerre elkeserítő és érdekes egyetemi története(i)m, szeretném kérni, hogy mind a nevem, mind bármilyen más adatom, elérhetőségem maradjon rejtve a nyilvánosság előtt, ugyanis tartok tőle, hogy ezen levelem kitudódása több elégtelen vizsgát és tanárokkal történő kellemetlen beszélgetést vonzana magával.
+
+Először is azt kívánom elmondani, hogy nagyon örülök, hogy végre előtérbe kerül a MAGYAR oktatás szabadsága. Mert persze a CEU-s tüntetéseken mindenki ott van, ahogy azzal is rengetegen foglalkoznak, hogy lesz-e gender-szak Magyarországon, de ki törődik azzal, hogy mi megy az állami pénzen működő, többségében magyarokat oktató, magyar egyetemeken?!
+
+Sok ismerősöm panaszkodik ezzel kapcsolatban a BCE-ről is, én azonban - saját vonatkozásom végett - az ELTE-ÁJK-t szeretném megemlíteni.
+
+A karon rengeteg a liberális szellemiségű, mind a hallgatók, mind az oktatók körében. Ezzel alapvetően semmi gond nem lenne. Ez mindenkinek a saját döntése, önmagában a politikai meggyőződésükkel nem sértenek másokat. Azonban amikor mindenki, akinek jobboldali és/vagy konzervatív gondolatai vannak, nácinak van bélyegezve sok hallgató körében, felmerül bennem a kérdés, milyen kifejezést használnának arra, akinek valóban nemzetiszocialista ambíciói vannak? A jogász tantervben szerepelnek a politológia, a magyar állam- és jogtörténet, valamint az összehasonlító jogtörténet tárgyak is. Ezek során megismerkedhetünk különböző kormányzati rendszerekkel, állami berendezkedésekkel, illetve nem szabad megfeledkezni arról sem, hogy a hallgatói jogviszony megkezdésének a jogász szakon előfeltétele az - emelt szintű - történelem érettségi. Ezek ismeretében véleményem szerint egy hallgatónak el kell tudnia határolni egy jobboldali kormányzást a diktatúrától. Ennek ellenére mégis gyakran hallom a folyosókon, olvasom Facebook-csoportokban, hogy "Viktátor", "nincs már nálunk semmi demokrácia", "diktatúrában élünk" stb.
+
+A másik, véleményem szerint rendkívül etikátlan viselkedés az egyetemen tanulók részéről az, amikor "ELTE-ÁJK" feliratú pulóverekben mennek tüntetni. Úgy gondolom, hogy a kart a nyilvánosság előtt képviselni elsősorban a dékán, és legvégső esetben sem a hallgatok kompetenciája, pláne úgy, hogy a kifejtett álláspontjukkal más hallgatótársaik nem értenek egyet.
+
+De ideje áttérni a tanárokra, illetve az oktatásra...
+
+Szeretném leszögezni, hogy rengeteg kiváló szaktekintély és jó ember van a karon, akik igyekeznek a fejünkbe verni a jogtudományokat, ugyanakkor az oktatás sokszor nem éppen a tudás átadásáról, illetve a "szabad művészetekről" szól.
+
+Nem tartom helyesnek, hogy egy alkotmányjog előadás 50%-ban a tananyag leadásáról, 50%-ban pedig arról szól, hogy a mai magyar vezetés mit nem csinál alkotmányosan, mi és hol ütközik demokratikus alapelvekbe, mi tarthatatlan stb. A burkolt célozgatások állandóak, de gyakori az oktatók politikai álláspontjainak - nyílt - hangoztatása is. Ezek pedig egyfajta liberális ekhóként is felfoghatóak, ugyanis kizárólag a baloldal álláspontját képviselik. Ez nem azért van így, mert a szakon minden oktató szíve balra húz, hanem mert a jobboldaliak - ahogy a hallgatók, úgy az oktatók körében is - kisebbségben vannak, és a konfliktusokat és az antipátiát elkerülvén inkább kihagyják az előadásukból a politikai töltetet (vagy csak egyszerűen nem tartják etikusnak, hogy a saját véleményükkel befolyásoljál és esetlegesen átformálják a hallgatókét).
+
+Sokszor hallhatjuk a különféle jogszabályok - elsősorban törvények - kritikáját, külön kiemelve Magyarország Alaptörvényét, melynek konstans leminősítése és szidalmazása gyakorlatilag a hétköznapok része az egyetemi berkekben. Olyan is elhangzott már azonban (más jogszabállyal összefüggésben), hogy „...habár ez a törvényben így van, én ezzel nem értek egyet, ezt ne így tanulják meg!" Ezzel összefüggésbe hozható az a nem ritka gyakorlat, hogy az előadáson elhangzottak ismerete prioritást élvez az írott jogi normákkal szemben - de legalábbis egyenrangú azokkal.
+
+Az Alkotmányjog 3 tárgynál biztatják tipikusan azzal a hallgatókat, hogy nem nehéz teljesíteni, hiszen ha jól megindokolja valaki a véleményét a feladat során, az el lesz fogadva. Ez majdnem teljes egészében így van, kiegészíteném azonban azzal, hogy AMENNYIBEN ez a vélemény egyezik az oktató/tanszék álláspontjával. Ha a jogeset úgy kezdődött, hogy XY kiment tüntetni, akkor már lehetett tudni, hogy a megoldás az lesz, hogy sérült a gyülekezéshez, valamint a véleménynyilvánítás szabadságához fűződő joga, a rendőrség pedig - természetesen, rendőrállamhoz méltó módon - jogtalanul, esetleg jogtalan erőszakot alkalmazva járt el.
+
+Az alkotmányjogot egy rendkívül érdekes tárgynak tartom, és függetlenül a leírt eseményektől fontosnak gondolom az alapos oktatását, valamint a megszerezhető tudás lehető legátfogóbb elsajátítását. Van azonban egy olyan tárgy, ami szinte kizárólag a liberális gondolatokra épül, nem tűrve semmilyen politikai "ellen gondolatot"; a jogszociológia. Habár a vizsgára felületesen át kell olvasni néhány szerző művét különböző témákban, a szemináriumok szinte kizárólag hazánk "antidemokratikus" működéséről és a liberális alapelvekről szólnak.
+
+Habár sok tanáron, professzoron ott van az "I stand with CEU" kitűző és "kiállnak az oktatás szabadságáért", továbbá "küzdenek" a demokráciáért és a szabad véleménynyilvánításért, ha valaki nem ért egyet velük, valamilyen szintű hátrányos megkülönböztetésre jó eséllyel számíthat mind az oktatói, mind a hallgatótársai részéről, annak ellenére, hogy a politikai vélemény alapján történő megkülönböztetés tilalmát Magyarország Alaptörvényének XV. cikk (2) bekezdése is kimondja.
+'''
+
+blob1 = TextBlob(text1)
+blob2 = TextBlob(text2)
+
+blob_888 = TextBlob(text_888)
+
+def smiley(blob):
+	print("--")
+	if blob.sentiment.polarity<0:
+		print(":(")
+	if blob.sentiment.polarity==0:
+		print(":|")
+	if blob.sentiment.polarity>0:
+		print(":)")
+	print(blob.sentiment.polarity)
+	print("--")
+
+for i in blob_888.sentences[:4]:
+	print(i)
+	i = i.translate(to="en")
+	for j in i.tags:
+		print(j)
+	smiley(i)
